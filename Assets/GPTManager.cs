83ee9739a0f87732a3937b82494b0cdb04670b32
MortezaFa <teezy10299@gmail.com>
2024-12-02 11:06:35 -0700

    Added ability to now send images to GPT 4o and get a repsonse back. Planning to add voice integration now

--- Assets/GPTManager.cs
+++ Assets/GPTManager.cs
@@ -1,42 +1,74 @@
+using System;
 using System.Collections.Generic;
+using Anaglyph.DisplayCapture;
 using OpenAI;
+using OpenAI.Chat;
+using OpenAI.Models;
 using UnityEngine;
 
 public class GptManager : MonoBehaviour {
 
-    private OpenAIApi openAI = new OpenAIApi();
-    private List<ChatMessage> messages = new List<ChatMessage>();
-
-
-    public async void GptTest() {
-        ChatMessage newMessage = new ChatMessage();
-        newMessage.Content = "Who are you";
-        newMessage.Role = "user";
-        
-        messages.Add(newMessage);
-        
-        CreateChatCompletionRequest request = new CreateChatCompletionRequest();
-        request.Messages = messages;
-        request.Model = "gpt-3.5-turbo";
-        
-        var response = await openAI.CreateChatCompletion(request);
-
-        if (response.Choices != null && response.Choices.Count > 0) {
-            var chatMessage = response.Choices[0].Message;
-            messages.Add(chatMessage);
-            Debug.Log("GPT response: " + chatMessage.Content); 
-            
-        }
-        
-        
+    private DisplayCaptureManager displayCaptureManager;
+    
+    
+    private void Start() {
+        GameObject displayCaptureManagerObject = GameObject.Find("Screen Capture Texture Manager");
+        displayCaptureManager = displayCaptureManagerObject.GetComponent<DisplayCaptureManager>();
     }
+
+    private String fetchBase64String() {
+        String base64Image = displayCaptureManager.convertCapturetoBase64();
+        return base64Image;
+    }
+
+    public async void sendPhotoToGPt(String prompt) {
+        var api = new OpenAIClient("");
+        var base64Image = fetchBase64String();
+        Debug.Log($"This is the prompt your sending meow:" + prompt);
+        var messages = new List<Message>
+        {
+            new Message(Role.System, "You are a helpful assistant."),
+            new Message(Role.User, new List<Content>
+            {
+                prompt,
+                new ImageUrl($"data:image/jpeg;base64,{base64Image}")
+            })
+        };
+        var chatRequest = new ChatRequest(messages, model: Model.GPT4o);
+        var response = await api.ChatEndpoint.GetCompletionAsync(chatRequest);
+        Debug.Log($"{response.FirstChoice.Message.Role}: {response.FirstChoice.Message.Content} | Finish Reason: {response.FirstChoice.FinishDetails}");
+        
+    
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private void Start() {
-    }
+
 
     // Update is called once per frame
     private void Update() {
